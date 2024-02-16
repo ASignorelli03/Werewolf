03e1d1f2e1004c56263e3bd97e3fb360a4294bb0
Anthony Signorelli <asignorelli@butler.edu>
2024-02-16 10:40:57 -0500

    test

--- Werewolf/MainMenu.Designer.cs
+++ Werewolf/MainMenu.Designer.cs
@@ -35,33 +35,51 @@
             this.settingsPlayerNameInput = new System.Windows.Forms.TextBox();
             this.btnPlayerAdder = new System.Windows.Forms.Button();
             this.LstPlayerNames = new System.Windows.Forms.ListBox();
+            this.tbctrlsettings = new System.Windows.Forms.TabControl();
+            this.tabPlayers = new System.Windows.Forms.TabPage();
+            this.tabRoles = new System.Windows.Forms.TabPage();
+            this.checkedListRoles = new System.Windows.Forms.CheckedListBox();
+            this.lblroledesc = new System.Windows.Forms.Label();
             this.menuPanel.SuspendLayout();
             this.settingsPanel.SuspendLayout();
+            this.tbctrlsettings.SuspendLayout();
+            this.tabPlayers.SuspendLayout();
+            this.tabRoles.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuPanel
             // 
+            this.menuPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.menuPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.menuPanel.Controls.Add(this.menuLabel);
             this.menuPanel.Controls.Add(this.menuPlayButton);
-            this.menuPanel.Location = new System.Drawing.Point(28, 15);
+            this.menuPanel.Location = new System.Drawing.Point(0, 0);
             this.menuPanel.Name = "menuPanel";
-            this.menuPanel.Size = new System.Drawing.Size(692, 374);
+            this.menuPanel.Size = new System.Drawing.Size(1423, 759);
             this.menuPanel.TabIndex = 0;
             // 
             // menuLabel
             // 
+            this.menuLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.menuLabel.AutoSize = true;
-            this.menuLabel.Location = new System.Drawing.Point(192, 69);
+            this.menuLabel.Location = new System.Drawing.Point(691, 254);
+            this.menuLabel.MaximumSize = new System.Drawing.Size(240, 60);
+            this.menuLabel.MinimumSize = new System.Drawing.Size(79, 20);
             this.menuLabel.Name = "menuLabel";
-            this.menuLabel.Size = new System.Drawing.Size(79, 20);
+            this.menuLabel.Size = new System.Drawing.Size(83, 20);
             this.menuLabel.TabIndex = 1;
-            this.menuLabel.Text = "WereWolf";
+            this.menuLabel.Text = "WereWolf ";
             // 
             // menuPlayButton
             // 
-            this.menuPlayButton.Location = new System.Drawing.Point(142, 149);
+            this.menuPlayButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.menuPlayButton.Location = new System.Drawing.Point(611, 304);
+            this.menuPlayButton.MaximumSize = new System.Drawing.Size(600, 225);
+            this.menuPlayButton.MinimumSize = new System.Drawing.Size(200, 75);
             this.menuPlayButton.Name = "menuPlayButton";
-            this.menuPlayButton.Size = new System.Drawing.Size(165, 75);
+            this.menuPlayButton.Size = new System.Drawing.Size(200, 75);
             this.menuPlayButton.TabIndex = 0;
             this.menuPlayButton.Text = "play";
             this.menuPlayButton.UseVisualStyleBackColor = true;
@@ -69,19 +87,20 @@
             // 
             // settingsPanel
             // 
-            this.settingsPanel.Controls.Add(this.LstPlayerNames);
-            this.settingsPanel.Controls.Add(this.btnPlayerAdder);
-            this.settingsPanel.Controls.Add(this.settingsPlayerNameInput);
-            this.settingsPanel.Location = new System.Drawing.Point(12, 12);
+            this.settingsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.settingsPanel.Controls.Add(this.tbctrlsettings);
+            this.settingsPanel.Location = new System.Drawing.Point(0, 0);
             this.settingsPanel.Name = "settingsPanel";
-            this.settingsPanel.Size = new System.Drawing.Size(627, 359);
+            this.settingsPanel.Size = new System.Drawing.Size(1423, 759);
             this.settingsPanel.TabIndex = 1;
             this.settingsPanel.Visible = false;
             this.settingsPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.settingsPanel_Paint);
             // 
             // settingsPlayerNameInput
             // 
-            this.settingsPlayerNameInput.Location = new System.Drawing.Point(119, 63);
+            this.settingsPlayerNameInput.Location = new System.Drawing.Point(166, 28);
             this.settingsPlayerNameInput.Name = "settingsPlayerNameInput";
             this.settingsPlayerNameInput.Size = new System.Drawing.Size(168, 26);
             this.settingsPlayerNameInput.TabIndex = 1;
@@ -89,7 +108,8 @@
             // 
             // btnPlayerAdder
             // 
-            this.btnPlayerAdder.Location = new System.Drawing.Point(119, 27);
+            this.btnPlayerAdder.AutoSize = true;
+            this.btnPlayerAdder.Location = new System.Drawing.Point(32, 26);
             this.btnPlayerAdder.Name = "btnPlayerAdder";
             this.btnPlayerAdder.Size = new System.Drawing.Size(115, 30);
             this.btnPlayerAdder.TabIndex = 2;
@@ -99,27 +119,151 @@
             // 
             // LstPlayerNames
             // 
+            this.LstPlayerNames.ColumnWidth = 75;
             this.LstPlayerNames.FormattingEnabled = true;
             this.LstPlayerNames.ItemHeight = 20;
-            this.LstPlayerNames.Location = new System.Drawing.Point(119, 98);
+            this.LstPlayerNames.Location = new System.Drawing.Point(32, 66);
+            this.LstPlayerNames.MultiColumn = true;
             this.LstPlayerNames.Name = "LstPlayerNames";
-            this.LstPlayerNames.Size = new System.Drawing.Size(168, 264);
+            this.LstPlayerNames.Size = new System.Drawing.Size(682, 264);
             this.LstPlayerNames.TabIndex = 3;
             this.LstPlayerNames.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
             // 
+            // tbctrlsettings
+            // 
+            this.tbctrlsettings.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbctrlsettings.Controls.Add(this.tabPlayers);
+            this.tbctrlsettings.Controls.Add(this.tabRoles);
+            this.tbctrlsettings.Location = new System.Drawing.Point(108, 68);
+            this.tbctrlsettings.Name = "tbctrlsettings";
+            this.tbctrlsettings.SelectedIndex = 0;
+            this.tbctrlsettings.Size = new System.Drawing.Size(1188, 611);
+            this.tbctrlsettings.TabIndex = 4;
+            // 
+            // tabPlayers
+            // 
+            this.tabPlayers.Controls.Add(this.LstPlayerNames);
+            this.tabPlayers.Controls.Add(this.btnPlayerAdder);
+            this.tabPlayers.Controls.Add(this.settingsPlayerNameInput);
+            this.tabPlayers.Location = new System.Drawing.Point(4, 54);
+            this.tabPlayers.Name = "tabPlayers";
+            this.tabPlayers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPlayers.Size = new System.Drawing.Size(1180, 553);
+            this.tabPlayers.TabIndex = 0;
+            this.tabPlayers.Text = "players";
+            this.tabPlayers.UseVisualStyleBackColor = true;
+            this.tabPlayers.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabRoles
+            // 
+            this.tabRoles.AutoScroll = true;
+            this.tabRoles.Controls.Add(this.lblroledesc);
+            this.tabRoles.Controls.Add(this.checkedListRoles);
+            this.tabRoles.Location = new System.Drawing.Point(4, 54);
+            this.tabRoles.Name = "tabRoles";
+            this.tabRoles.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRoles.Size = new System.Drawing.Size(1180, 553);
+            this.tabRoles.TabIndex = 1;
+            this.tabRoles.Text = "roles";
+            this.tabRoles.UseVisualStyleBackColor = true;
+            this.tabRoles.Click += new System.EventHandler(this.tabRoles_Click);
+            // 
+            // checkedListRoles
+            // 
+            this.checkedListRoles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListRoles.ColumnWidth = 175;
+            this.checkedListRoles.FormattingEnabled = true;
+            this.checkedListRoles.Items.AddRange(new object[] {
+            "The Amulet of Protection:",
+            "Apprentice Seer (+4): ",
+            "Aura Seer (+3): ",
+            "Beholder (+2): ",
+            "Big Bad Wolf (-9):",
+            "Bogeyman (-6): ",
+            "Bodyguard (+3): ",
+            "Cupid (-3): ",
+            "The Count (+5): ",
+            "Diseased (+3): ",
+            "Fruit Brute (-3):",
+            "Ghost (+2):",
+            "Hunter (+3): ",
+            "Village Idiot (+2):",
+            "Insomniac (+3):",
+            "Lycan (-1):",
+            "Wolf man (-9) : ",
+            "Martyr (+3):",
+            "Mason (+2) (3): ",
+            "Mayor (+2): ",
+            "Old Hag (+1): ",
+            "Old Man (0):",
+            "P.I. (+3): ",
+            "Pacifist (-2): ",
+            "Priest (+3): ",
+            "Prince (+3): ",
+            "Seer (+7): ",
+            "Spellcaster (+1):",
+            "Tough Guy (+3): ",
+            "Troublemaker (+2): ",
+            "Thing (+3): ",
+            "Villager (+1) (20): ",
+            "Witch (+4):",
+            "Sorcerer (-3): ",
+            "Minion (-6): ",
+            "Werewolf (-6) (12): ",
+            "Wolf Cub (-8):",
+            "Dream Wolf (-5): ",
+            "Cursed (-3): ",
+            "Doppelgänger (-2): ",
+            "Drunk (3): ",
+            "Cult Leader (+1): ",
+            "Hoodlum:",
+            "Tanner (+1):",
+            "Lone Wolf (-5): ",
+            "Vampire (-7) (8): ",
+            "Wild Child: ",
+            "Sasquatch (-2): ",
+            "Leprechaun (+5): ",
+            "Bloody Mary (+1): ",
+            "Chupacabra (+4): ",
+            "Nostradamus (+1): ",
+            "Dire Wolf (-4): "});
+            this.checkedListRoles.Location = new System.Drawing.Point(55, 38);
+            this.checkedListRoles.MultiColumn = true;
+            this.checkedListRoles.Name = "checkedListRoles";
+            this.checkedListRoles.Size = new System.Drawing.Size(1026, 372);
+            this.checkedListRoles.TabIndex = 0;
+            this.checkedListRoles.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // lblroledesc
+            // 
+            this.lblroledesc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblroledesc.Location = new System.Drawing.Point(51, 443);
+            this.lblroledesc.Name = "lblroledesc";
+            this.lblroledesc.Size = new System.Drawing.Size(1000, 75);
+            this.lblroledesc.TabIndex = 1;
+            // 
             // MainMenu
             // 
+            this.AcceptButton = this.btnPlayerAdder;
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.settingsPanel);
+            this.ClientSize = new System.Drawing.Size(1423, 759);
             this.Controls.Add(this.menuPanel);
+            this.Controls.Add(this.settingsPanel);
             this.Name = "MainMenu";
+            this.ShowIcon = false;
             this.Text = "MainMenu";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainMenu_Load);
             this.menuPanel.ResumeLayout(false);
             this.menuPanel.PerformLayout();
             this.settingsPanel.ResumeLayout(false);
-            this.settingsPanel.PerformLayout();
+            this.tbctrlsettings.ResumeLayout(false);
+            this.tabPlayers.ResumeLayout(false);
+            this.tabPlayers.PerformLayout();
+            this.tabRoles.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -133,5 +277,10 @@
         private System.Windows.Forms.TextBox settingsPlayerNameInput;
         private System.Windows.Forms.Button btnPlayerAdder;
         private System.Windows.Forms.ListBox LstPlayerNames;
+        private System.Windows.Forms.TabControl tbctrlsettings;
+        private System.Windows.Forms.TabPage tabPlayers;
+        private System.Windows.Forms.TabPage tabRoles;
+        private System.Windows.Forms.CheckedListBox checkedListRoles;
+        private System.Windows.Forms.Label lblroledesc;
     }
 }
--- Werewolf/MainMenu.cs
+++ Werewolf/MainMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
@@ -9,11 +10,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Werewolf
 {
     public partial class MainMenu : Form
     {
+
         public MainMenu()
         {
             InitializeComponent();
@@ -95,5 +99,77 @@ namespace Werewolf
         {
 
         }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+        }
+        private void MainMenu_Resize(object sender, System.EventArgs e)
+        {
+            menuPlayButton.Location = new Point(this.Width / 2 - menuPlayButton.Width / 2, this.Height / 2 - menuPlayButton.Height / 2);
+            menuPlayButton.Size = new Size(this.Width / 4, this.Height / 6);
+            menuLabel.Location = new Point(this.Width / 2 - menuLabel.Width / 2, this.Height / 4);
+            menuLabel.Size = new Size(120, 20);
+
+           
+            
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //find description of currect selected role
+            for(int i = 0;i<25;i++)
+            {
+                if (roleTable[i,0].Contains(checkedListRoles.SelectedIndex.ToString()))
+                {
+                    lblroledesc.Text = roleTable[i,3];
+                    lblroledesc.Update();
+
+                }
+            }
+        }
+
+        private void tabRoles_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtbxRoleDesc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        String[,] roleTable =
+        {
+            {"Apprentice Seer","villager","4" ,"Become the Seer if the Seer is killed." },
+            {"Bodyguard","villager","3" ,"Choose a different player each night to protect. That player cannot be killed that night."},
+            {"Cupid","villager","-3","Choose two players to be lovers. If one of those players dies, the other dies from a broken heart." },
+            {"Diseased","villager","3","If you are attacked by werewolves, the werewolves do not get fed the following night." },
+            {"Hunter","villager","3","If you are killed, take someone down with you." },
+            {"Village Idiot","village","2","Always vote for players to die." },
+            {"Lycan","villager","-1","You are a villager, but you appear falsely to be a werewolf to the Seers and PI." },
+            {"Wolf man","werewolf","-9","You Wake With the other Werewolves each night, but the Seer sees you as a Villager" },
+            {"Mayor","villager","2","Your vote counts twice when voting to lynch a player if you reveal yourself." },
+            {"Old Hag","villager","1","At night, indicate a player who must leave the village the next day." },
+            {"P.I.","villager","3","Inspect three players each night (they must be beside each other). You only know if at least one of them is malicious. (variation: Inspect three adjacent players on one night)." },
+            {"Priest","villager","3","On the first night, protect a player. The next attempt to kill the player fails. The night after that attempt, you protect a different player. (variation: Protect one player from death caused at night, including vampire attacks.)" },
+            {"Prince","villager","3","You can’t be lynched." },
+            {"Seer","villager","7","Each night, point at a player and learn if they are: Either on the villager team, or a vampire, or, if a werewolf, the exact powers." },
+            {"Spellcaster","villager","1" ,"At night, indicate a player who must not use their voice the following day." },
+            {"Tough Guy","villager","3","You survive an extra day if attacked by werewolves at night." },
+            {"Villager","villager","1","Find the werewolves and lynch them." },
+            {"Witch","villager","4","Kill or heal a player, once each per game." },
+            {"Sorcerer","werewolf","-3","You are a seer, but you are on the Werewolf team. You only know if you’ve found a werewolf, another seer, or something else." },
+            {"Minion","werewolf","-6","Work with the werewolves or vampires to kill the villagers. The moderator decides whether you work with the werewolves or the vampires." },
+            {"Werewolf","werewolf","-6","Eat a villager each night." },
+            {"Wolf Cub","werewolf","-8","If you die, the werewolves get two kills the following night." },
+            {"Cursed","neutral","-3","You are a villager until attacked by werewolves, at which time you become a werewolf. (variation: You become a vampire when attacked by vampires.)" },
+            {"Doppelgänger","neutral","-2","Select a player the first night. If that player dies, you secretly take that role." },
+            {"Drunk","villager","3","You are a villager until the third night, when you remember your real role." }, 
+           //incomplete list https://ultimatewerewolfgames.tumblr.com/roles
+        };
     }
 }
